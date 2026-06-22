391191e2448e4c5b3e26aeb486c5bfb16511d9e7
j124klo <cioskradek@gmail.com>
2026-06-22 22:20:12 +0200

    initial commit

---
+++ src/test/java/pl/polsl/przychodnia/PrzychodniaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.przychodnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzychodniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
